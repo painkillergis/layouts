25ac9417f23742dbe45891a3446da9de8fae58f0
tyler <tyler@cruftbusters.com>
2021-02-05 22:54:02 -0600

    tile request breaks dirtily into tiles

--- src/main/kotlin/com/painkillergis/layouts/tiles/TileService.kt
+++ src/main/kotlin/com/painkillergis/layouts/tiles/TileService.kt
@@ -1,19 +1,32 @@
 package com.painkillergis.layouts.tiles
 
 import com.painkillergis.layouts.Rectangle
+import java.lang.Integer.min
 
 class TileService {
   fun answer(question: TileQuestion): List<Tile> {
-    return (0 until question.size.height / question.tileSize.height).flatMap { y ->
-      (0 until question.size.width / question.tileSize.width).map { x ->
+    val (totalSize, tileSize) = question
+    return getIndexTiles(question)
+      .map { (x, y) -> Rectangle(x * tileSize.width, y * tileSize.height) }
+      .map { position ->
         Tile(
-          size = question.tileSize,
-          position = Rectangle(
-            x * question.tileSize.width,
-            y * question.tileSize.height,
+          Rectangle(
+            min(tileSize.width, totalSize.width - position.width),
+            min(tileSize.height, totalSize.height - position.height),
           ),
+          position,
         )
       }
-    }
+  }
+
+  private fun getIndexTiles(question: TileQuestion): List<Pair<Int, Int>> {
+    val (size, tileSize) = question
+    val xs = if (size.width % tileSize.height > 0)
+      0..(size.width / tileSize.width)
+    else 0 until size.width / tileSize.width
+    val ys = if (size.height % tileSize.height > 0)
+      0..(size.height / tileSize.height)
+    else 0 until size.height / tileSize.height
+    return ys.flatMap { y -> xs.map { Pair(it, y) } }
   }
 }
--- src/test/kotlin/com/painkillergis/layouts/tiles/TileServiceSpec.kt
+++ src/test/kotlin/com/painkillergis/layouts/tiles/TileServiceSpec.kt
@@ -44,4 +44,30 @@ internal class TileServiceSpec : StringSpec({
       ),
     )
   }
+
+  "tile request breaks dirtily into tiles" {
+    TileService().answer(
+      TileQuestion(
+        size = Rectangle(10, 10),
+        tileSize = Rectangle(6, 6),
+      ),
+    ) shouldBe listOf(
+      Tile(
+        size = Rectangle(6, 6),
+        position = Rectangle(0, 0),
+      ),
+      Tile(
+        size = Rectangle(4, 6),
+        position = Rectangle(6, 0),
+      ),
+      Tile(
+        size = Rectangle(6, 4),
+        position = Rectangle(0, 6),
+      ),
+      Tile(
+        size = Rectangle(4, 4),
+        position = Rectangle(6, 6),
+      ),
+    )
+  }
 })
